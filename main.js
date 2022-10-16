daf87fafbae4b836a2b81d119194cd136b420776
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-10-16 17:15:16 +0200

    add logic for task modal

--- main.js
+++ main.js
@@ -1,6 +1,75 @@
+/* MODAL */
+const addTaskModal = document.getElementById('add-modal')
+const addTaskButton = document.getElementById('main-header').lastElementChild;
+const backdrop = document.getElementById('backdrop') 
+const cancelAddTaskButton = addTaskModal.querySelector('.btn--passive');
+const confirmAddTaskButton = cancelAddTaskButton.nextElementSibling
+const userInputs = addTaskModal.querySelectorAll('input')
 
+const tasks = []
 
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible')
+}
 
+const closeTaskModal = () => {
+  addTaskModal.classList.remove('visible')
+}
+
+const showTaskModal = () => {
+  addTaskModal.classList.add('visible')
+  toggleBackdrop()
+}
+
+const clearTaskInput = () => {
+  for (const usrInput of userInputs) {
+    usrInput.value = '';
+  }
+};
+
+const cancelAddTaskHandler = () => {
+  closeTaskModal()
+  toggleBackdrop()
+  clearTaskInput()
+}
+
+const addTaskHandler = () => {
+  const titleValue = userInputs[0].value;
+  const descriptionValue = userInputs[1].value;
+  const extraValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === '' ||
+    descriptionValue.trim() === '' ||
+    extraValue.trim() === ''
+  ) {
+    alert('Please enter valid values.');
+    return;
+  }
+
+  const newTask = {
+    title: titleValue,
+    description: descriptionValue,
+    extraInfo: extraValue
+  }
+
+  tasks.push(newTask)
+  console.log(tasks);
+  closeTaskModal()
+  toggleBackdrop()
+  clearTaskInput()
+};
+
+const backdropClickHandler = () => {
+  closeTaskModal()
+  toggleBackdrop()
+  clearTaskInput()
+}
+
+addTaskButton.addEventListener('click', showTaskModal)
+backdrop.addEventListener('click', backdropClickHandler)
+cancelAddTaskButton.addEventListener('click', cancelAddTaskHandler)
+confirmAddTaskButton.addEventListener('click', addTaskHandler)
 
 
 /* EASTEREGG */
