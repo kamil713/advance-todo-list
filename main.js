d8315a9c6ee9eb05742f755fd6563b4c3822cace
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-10-19 19:51:42 +0200

    add todo logic

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 /* MODAL */
-
-/* TODO LOGIC */
-
 class Modal {
 	constructor() {
-		this.tasks = [];
 		this.addTaskModal = document.getElementById('add-modal');
 		this.addTaskButton =
 			document.getElementById('main-header').lastElementChild;
@@ -31,6 +27,7 @@ class Modal {
 		`;
 		const listRoot = document.querySelector(`#active-tasks ul`);
 		listRoot.append(newTaskElement);
+		newTaskElement.scrollIntoView({ behavior: 'smooth' });
 	}
 
 	closeTaskModal() {
@@ -77,8 +74,6 @@ class Modal {
 			extraInfo: extraValue,
 		};
 
-		this.tasks.push(newTask);
-		console.log(this.tasks);
 		this.closeTaskModal();
 		this.toggleBackdrop();
 		this.clearTaskInput();
@@ -87,6 +82,15 @@ class Modal {
 			newTask.title,
 			newTask.description,
 			newTask.extraInfo
+		);
+
+		const activeTasksList = new TaskList('active');
+		const finishedTasksList = new TaskList('finished');
+		activeTasksList.setSwitchHandlerFunction(
+			finishedTasksList.addTask.bind(finishedTasksList)
+		);
+		finishedTasksList.setSwitchHandlerFunction(
+			activeTasksList.addTask.bind(activeTasksList)
 		);
 	}
 
@@ -113,8 +117,181 @@ class Modal {
 	}
 }
 
+
+/* TODO LOGIC */
+class DOMHelper {
+	static clearEventListeners(element) {
+		const clonedElement = element.cloneNode(true);
+		element.replaceWith(clonedElement);
+		return clonedElement;
+	}
+
+	static moveElement(elementId, newDestinationSelector) {
+		const element = document.getElementById(elementId);
+		const destinationElement = document.querySelector(newDestinationSelector);
+		destinationElement.append(element);
+		element.scrollIntoView({ behavior: 'smooth' });
+	}
+}
+
+class Component {
+	constructor(hostElementId, insertBefore = false) {
+		if (hostElementId) {
+			this.hostElement = document.getElementById(hostElementId);
+		} else {
+			this.hostElement = document.body;
+		}
+		this.insertBefore = insertBefore;
+	}
+
+	detach() {
+		if (this.element) {
+			this.element.remove();
+		}
+	}
+
+	attach() {
+		this.hostElement.insertAdjacentElement(
+			this.insertBefore ? 'afterbegin' : 'beforeend',
+			this.element
+		);
+	}
+}
+
+class Tooltip extends Component {
+	constructor(closeNotifierFunction, text, hostElementId) {
+		super(hostElementId);
+		this.closeNotifier = closeNotifierFunction;
+		this.text = text;
+		this.create();
+	}
+
+	closeTooltip = () => {
+		this.detach();
+		this.closeNotifier();
+	};
+
+	create() {
+		const tooltipElement = document.createElement('div');
+		tooltipElement.className = 'task';
+		const tooltipTemplate = document.getElementById('tooltip');
+		const tooltipBody = document.importNode(tooltipTemplate.content, true);
+		tooltipBody.querySelector('p').textContent = this.text;
+		tooltipElement.append(tooltipBody);
+
+		const hostElPosLeft = this.hostElement.offsetLeft;
+		const hostElPosTop = this.hostElement.offsetTop;
+		const hostElHeight = this.hostElement.clientHeight;
+		const parentElementScrolling = this.hostElement.parentElement.scrollTop;
+
+		const x = hostElPosLeft + 20;
+		const y = hostElPosTop + hostElHeight - parentElementScrolling - 10;
+
+		tooltipElement.style.position = 'absolute';
+		tooltipElement.style.left = x + 'px'; // 500px
+		tooltipElement.style.top = y + 'px';
+
+		tooltipElement.addEventListener('click', this.closeTooltip);
+		this.element = tooltipElement;
+	}
+}
+
+class TaskItem {
+	hasActiveTooltip = false;
+
+	constructor(id, updateTaskListsFunction, type) {
+		this.id = id;
+		this.updateTaskListsHandler = updateTaskListsFunction;
+		this.connectMoreInfoButton();
+		this.connectSwitchButton(type);
+	}
+
+	showMoreInfoHandler() {
+		if (this.hasActiveTooltip) {
+			return;
+		}
+		const taskElement = document.getElementById(this.id);
+		const tooltipText = taskElement.dataset.extraInfo;
+		const tooltip = new Tooltip(
+			() => {
+				this.hasActiveTooltip = false;
+			},
+			tooltipText,
+			this.id
+		);
+		tooltip.attach();
+		this.hasActiveTooltip = true;
+	}
+
+	connectMoreInfoButton() {
+		const taskItemElement = document.getElementById(this.id);
+		const moreInfoBtn = taskItemElement.querySelector('button:first-of-type');
+		if (moreInfoBtn.getAttribute('listener') !== 'true') {
+			moreInfoBtn.addEventListener('click', () => {
+				this.showMoreInfoHandler();
+				moreInfoBtn.setAttribute('listener', 'true');
+			});
+		}
+	}
+
+	connectSwitchButton(type) {
+		const taskItemElement = document.getElementById(this.id);
+		let switchBtn = taskItemElement.querySelector('button:last-of-type');
+		switchBtn = DOMHelper.clearEventListeners(switchBtn);
+		switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
+		switchBtn.addEventListener(
+			'click',
+			this.updateTaskListsHandler.bind(null, this.id)
+		);
+	}
+
+	update(updateTaskListsFn, type) {
+		this.updateTaskListsHandler = updateTaskListsFn;
+		this.connectSwitchButton(type);
+	}
+}
+
+class TaskList {
+	tasks = [];
+
+	constructor(type) {
+		this.type = type;
+		const taskItems = document.querySelectorAll(`#${type}-tasks li`);
+		for (const taskItem of taskItems) {
+			this.tasks.push(
+				new TaskItem(taskItem.id, this.switchTask.bind(this), this.type)
+			);
+		}
+		console.log(this.tasks);
+	}
+
+	setSwitchHandlerFunction(switchHandlerFunction) {
+		this.switchHandler = switchHandlerFunction;
+	}
+
+	addTask(task) {
+		this.tasks.push(task);
+		DOMHelper.moveElement(task.id, `#${this.type}-tasks ul`);
+		task.update(this.switchTask.bind(this), this.type);
+	}
+
+	switchTask(taskId) {
+		this.switchHandler(this.tasks.find((t) => t.id === taskId));
+		this.tasks = this.tasks.filter((t) => t.id !== taskId);
+	}
+}
+
 class App {
 	static init() {
+		const activeTasksList = new TaskList('active');
+		const finishedTasksList = new TaskList('finished');
+		activeTasksList.setSwitchHandlerFunction(
+			finishedTasksList.addTask.bind(finishedTasksList)
+		);
+		finishedTasksList.setSwitchHandlerFunction(
+			activeTasksList.addTask.bind(activeTasksList)
+		);
+
 		const modal = new Modal();
 		modal.init();
 	}
